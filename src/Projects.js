f79dde5d5ef97d4855a24efa5d69a9bde6cab4c0
Ishaan-10 <b.ishaan10@gmail.com>
2021-09-05 22:58:32 +0530

    projectData

--- src/Projects.js
+++ src/Projects.js
@@ -1,22 +1,21 @@
 import React from 'react'
 import './Assets/Css/projects.css'
-import {Col, Row} from 'react-bootstrap'
+import { Col, Row } from 'react-bootstrap'
 import Cardone from './Cardone'
 import Cardtwo from './Cardtwo'
+import projectData from './projectdata'
 
 export default function Projects() {
     return (
         <div id="projects-container">
             <div className="container">
                 {/* <h2 className="heading">Projects</h2> */}
-                <Row>
-                    <Col md><Cardtwo /></Col>
-                    <Col md><Cardone /></Col>
-                </Row>
-                <Row>
-                    <Col md><Cardtwo /></Col>
-                    <Col md><Cardone /></Col>
-                </Row>
+                {projectData.map((project, index) => {
+                        return (<Row>
+                            <Col md><Cardtwo /></Col>
+                        </Row>)
+
+                })}
 
 
             </div>
---
+++ src/projectdata.js
@@ -0,0 +1,25 @@
+const projectData = [
+    {
+        title:"Financify",
+        date:"08/21",
+        description:"A web-app made using MERN stack which acts as a hub for all the transactions, subscriptions and goals a person wants to keep a track of. Record your transactions by just clicking a photo of the bill/receipt.",
+        tagline:"A full stack MERN app",
+        tags:["MongoDB,Express,React,Node.js"],
+        liveLink:"https://financify-zeta.netlify.app/",
+        githubLink:"https://github.com/Ishaan-10/FINANCIFY",
+        image:"https://raw.githubusercontent.com/Ishaan-10/FINANCIFY/main/client/src/images/landing.png",
+    },
+    {
+        title:"Financify",
+        date:"10/10/12",
+        description:"",
+        tagline:"",
+        tags:["MongoDB,Express,React,Node.js"],
+        liveLink:"",
+        githubLink:"",
+        image:"",
+    },
+
+]
+
+export default projectData;
