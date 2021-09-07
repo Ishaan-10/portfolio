454f728289caef7fc88b19b71cfd5859fff2e119
Ishaan-10 <b.ishaan10@gmail.com>
2021-09-07 10:36:18 +0530

    contact form

--- src/Contact.js
+++ src/Contact.js
@@ -1,9 +1,59 @@
 import React from 'react'
+import './Assets/Css/contact.css'
+import { Container, Row } from 'react-bootstrap'
 
 export default function Contact() {
+
+    const resetForm = ()=>{
+        document.querySelectorAll("input").value="";
+    }
+
     return (
         <div class="contact-container">
-            
+            <Container className="pt-3">
+                <Row>
+                    <form class="my-form" netlify>
+                        <div class="container">
+                            <ul>
+                                <h2>Get in touch</h2>
+                                <h6 className="p-2">Will get back to you as soon as possible!</h6>
+                                <li>
+                                    <div class="grid grid-2">
+                                        <input type="text" placeholder="Name" required />
+                                        <input type="text" placeholder="Surname" required />
+                                    </div>
+                                </li>
+                                <li>
+                                    <div class="grid grid-2">
+                                        <input type="email" placeholder="Email" required />
+                                        <input type="tel" placeholder="Phone" />
+                                    </div>
+                                </li>
+                                <li>
+                                    <textarea placeholder="Message"></textarea>
+                                </li>
+                                <li>
+                                    <div class="grid grid-3">
+                                        <div class="required-msg">REQUIRED FIELDS</div>
+                                        <button class="btn-grid" type="submit">
+                                            <span class="back">
+                                                <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/email-icon.svg" alt="" />
+                                            </span>
+                                            <span class="front">SUBMIT</span>
+                                        </button>
+                                        <button class="btn-grid" onClick={resetForm} type="reset">
+                                            <span class="back">
+                                                <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/eraser-icon.svg" alt="" />
+                                            </span>
+                                            <span class="front">RESET</span>
+                                        </button>
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
+                    </form>
+                </Row>
+            </Container>
         </div>
     )
 }
